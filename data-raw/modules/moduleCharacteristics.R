8ed692a082786f20df3c2c83119676353482ffa5
catalamarti <marticatalasabate@gmail.com>
2023-08-06 19:31:29 +0200

    implement read data

--- data-raw/modules/moduleCharacteristics.R
+++ data-raw/modules/moduleCharacteristics.R
@@ -1,5 +1,7 @@
 # This file contains the information for the incidence module
 moduleCharacteristics <- list(
+  packages = c("readr", "here"),
+  read = c("Summary characteristics"),
   menu = dplyr::tibble(
     item = c("Characteristics"), sub_item = c("Individual characteristics")
   )
